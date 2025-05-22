1edacf1181fb0f0d542cd83f08ca5914759d6b02
Akhil Singh <akhilsingh52.as@gmail.com>
2025-05-22 18:57:08 +0530

    Refactor Report class to inherit from Base and update generated_at field initialization

--- Models/report.py
+++ Models/report.py
@@ -1,12 +1,13 @@
 from dataclasses import dataclass, field
 from datetime import datetime
 
+from Models.base import Base
 from utils.time import get_morning_time
 
 
 @dataclass
-class Report:
-    generated_at: datetime = field(default_factory=lambda: get_morning_time)
+class Report(Base):
+    generated_at: datetime = field(default_factory=get_morning_time)
     num_followers: int = 0
     num_following: int = 0
     users: list[dict] = field(default_factory=list)
